c5800a9a3f4d18944713f85e9bc3fbba3125d916
shivam sharma <shivamsharma0887@gmail.com>
2024-02-14 19:58:51 +0530

    fix billing url

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,5 +7,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export function absoluteUrl(path: string) {
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`;
-  return `http://localhost:${process.env.PORT ?? 3000}${path}`;
+  else if (typeof window === undefined)
+    `http://localhost:${process.env.PORT ?? 3000}${path}`;
+  return path;
 }
